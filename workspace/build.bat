45fd3f473a9dad28126e86d7a83aeed0a5d1f844
Istar Eldritch <me@ruben.io>
2024-03-30 04:05:01 +0000

    Fix build script

--- workspace/build.bat
+++ workspace/build.bat
@@ -1,14 +1,16 @@
 
 set "SCRIPT_DIR=%cd%"
 set "DAYZ_PATH=%ProgramFiles(x86)%\Steam\steamapps\common\DayZ\"
-set "ADDON_BUILDER_PATH=%ProgramFiles(x86)%\Steam\steamapps\common\DayZ Tools\Bin\AddonBuilder"
+set "ADDON_BUILDER_PATH=%ProgramFiles(x86)%\Steam\steamapps\common\DayZ Tools\Bin\AddonBuilder\AddonBuilder.exe"
 
 set "MOD_PATH=P:\Mods\@IEF"
-rmdir /s "%MOD_PATH%"
+rmdir /s /q "%MOD_PATH%"
 md "%MOD_PATH%/addons"
-xcopy /s "%SCRIPT_DIR%/../keys" "%MOD_PATH%/keys"
+xcopy /s /i "%SCRIPT_DIR%/../keys" "%MOD_PATH%/keys"
 copy "%SCRIPT_DIR%/../LICENSE" "%MOD_PATH%/LICENSE"
 copy "%SCRIPT_DIR%/../README.md" "%MOD_PATH%/README.md"
 copy "%SCRIPT_DIR%/../mod.cpp" "%MOD_PATH%/mod.cpp"
 
-start /wait /D "%ADDON_BUILDER_PATH%" AddonBuilder.exe "P:\IE\Framework" "%MOD_PATH%\addons" -project="P:" -prefix="IE/Framework" -clear -binarizeFullLogs -sign="%UserProfile%\Documents\Keys\IstarEldritch.biprivatekey"
+set "includes=%SCRIPT_DIR%\buildincludes"
+
+"%ADDON_BUILDER_PATH%" "P:\IE\Framework" "%MOD_PATH%\addons" -project="P:" -prefix="IE/Framework" -include=%includes% -clear -binarizeFullLogs -sign="%UserProfile%\Documents\Keys\IstarEldritch.biprivatekey"
