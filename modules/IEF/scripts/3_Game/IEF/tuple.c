9392412a86a58a34c6803ed01fe97af1a07d667e
Istar Eldritch <me@ruben.io>
2024-04-25 21:26:49 +0100

    Add up to Tuple4

---
+++ modules/IEF/scripts/3_Game/IEF/tuple.c
@@ -0,0 +1,41 @@
+class IE_Tuple2<Class T1, Class T2>
+{
+	T1 t1;
+	T2 t2;
+
+	void IE_Tuple2(T1 _t1, T2 _t2)
+	{
+		t1 = _t1;
+		t2 = _t2;
+	}
+}
+
+class IE_Tuple3<Class T1, Class T2, Class T3>
+{
+	T1 t1;
+	T2 t2;
+    T3 t3;
+
+	void IE_Tuple3(T1 _t1, T2 _t2, T3 _t3)
+	{
+		t1 = _t1;
+		t2 = _t2;
+        t3 = _t3;
+	}
+}
+
+class IE_Tuple4<Class T1, Class T2, Class T3, Class T4>
+{
+	T1 t1;
+	T2 t2;
+    T3 t3;
+    T4 t4;
+
+	void IE_Tuple4(T1 _t1, T2 _t2, T3 _t3, T4 _t4)
+	{
+		t1 = _t1;
+		t2 = _t2;
+        t3 = _t3;
+        t4 = _t4;
+	}
+}
